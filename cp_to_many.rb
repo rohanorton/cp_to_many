80c77105c7f305b6d44849e2bf98c0d7a6238812
Rohan Orton <rohan.orton@gmail.com>
2014-12-04 12:54:12 +0000

    added error handling

--- cp_to_many.rb
+++ cp_to_many.rb
@@ -2,6 +2,17 @@
 require "fileutils"
 require "optparse"
 
+def die(error_type, src = nil)
+    get_help = "\nTry 'cp_to_many.rb --help' for more information"
+    error_message = {
+        :no_src => "missing file operand #{get_help}",
+        :cannot_stat => "cannot stat '#{src}': no suce file or directory",
+        :no_dest => "missing destination file operand after '#{src}' #{get_help}"
+    }
+    puts "cp_to_many: #{error_message[error_type]}"
+    exit 1
+end
+
 options = {}
 cp_options = {}
 
@@ -31,9 +42,16 @@ OptionParser.new do|opts|
 end.parse!
 
 src = ARGV.shift()
+if !src then  die(:no_src) end
+
 dests = ARGV
+if dests.length == 0 then die(:no_dest, src) end
 
-
+begin 
+    File.stat(src)
+rescue
+    die(:cannot_stat, src)
+end
 
 for dest in dests
     # TODO:
